544c32956448750c20efee1bdba0b6dc7c6c74e9
Robert Black <robertblack@Roberts-MacBook-Air.local>
2016-03-07 00:42:11 -0500

    pages added

--- gif.js
+++ gif.js
@@ -18,19 +18,32 @@ jQuery(document).ready(function() {
 		};
 	};
 	$("body").on("click", ".gifButton", function(){
-		var input = $(this).attr("id");
-		var url = "https://api.giphy.com/v1/gifs/search?q="+input+"&api_key=dc6zaTOxFJmzC "; 
+		popGifs(0, $(this).attr("id"));
+	});
+	function popGifs(number, input){
+		var reference = parseInt(number);
+		var url = "https://api.giphy.com/v1/gifs/search?q="+input+"&limit=100&api_key=dc6zaTOxFJmzC "; 
 		$.ajax({url: url, method: "GET"}).done(function(response) {
 			$("#gifs").html("");
-			console.log(response);
-			for(var i=0; i<11; i++){
+			for(var i=reference; i<reference+10; i++){
 				var still = response.data[i].images.fixed_height_still.url;
 				var gif = response.data[i].images.fixed_height.url;
 				var rating = "<div class='rating'>Rating: " + response.data[i].rating +"</div>";
-				$("#gifs").append("<div class='gifDiv text-center thumbnail'>"+rating+"<div class='gif' data-still='"+still+"' data-gif='"+gif+"' data-state='still'><img src='"+still+"'></div></div>");
+				$("#gifs").append("<span class='gifDiv text-center thumbnail'>"+rating+"<div class='gif' data-still='"+still+"' data-gif='"+gif+"' data-state='still'><img src='"+still+"' class='img-responsive center-block'></div></span>");
+			};
+			if(response.data.length > 10){
+				var pages = (response.data.length - (response.data.length % 10)) / 10;
+				if(response.data.length % 10 != 0) pages++;
+				$("#pages").html("<span style='font-size: 24px; font-weight: bold;'>Pages: </span>");
+				for(i=1; i<=pages; i++){
+					$("#pages").append("<span class='pageButton' data-input='"+input+"' data-num='"+((i-1)*10)+"'><button type='button' class='btn button'>"+i+"</button></span>");
+				};
 			};
     	});
-	});
+    };
+    $("body").on("click", ".pageButton", function(){
+    	popGifs($(this).attr("data-num"), $(this).attr("data-input"));
+    });
 	$("body").on("click", ".gif", function(){
 		if($(this).attr("data-state") == "still"){
 			$(this).attr("data-state", "animate");
